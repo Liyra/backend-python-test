fbbf60bd04c83c401f5a283d97c71d4e6edf7e96
Alexandre Deneuve <alexandre.deneuve@outlook.com>
2019-03-10 14:03:23 -0400

    Remove useless db relationship

--- alayatodo/models.py
+++ alayatodo/models.py
@@ -4,7 +4,6 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), nullable=False)
     password = db.Column(db.String(255), nullable=False)
-    todos = db.relationship('Todos', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<Users {}>'.format(self.username)
