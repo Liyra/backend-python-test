1ca426ad88979aa7874a1e4b8fa5fcb794a23f77
Alexandre Deneuve <alexandre.deneuve@outlook.com>
2019-03-14 18:48:15 -0400

    Add uniqueness on username column

--- resources/task_2_migration.sql
+++ resources/task_2_migration.sql
@@ -1,5 +1,19 @@
+--changeset adeneuve:1
 ALTER TABLE todos
   ADD completed INTEGER(1);
 
 UPDATE todos
 SET completed=0;
+
+--changeset adeneuve:2
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  username VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+INSERT INTO users (username, password) VALUES
+('user1', 'user1'),
+('user2', 'user2'),
+('user3', 'user3');
